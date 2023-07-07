697d361a5c4dc99de92f6157193c7d3d891e6271
zaineme <zaineraybautista@gmail.com>
2023-07-07 18:56:56 +0800

    removeFromArray commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(origArray, ...args) {
+    for (let index = 0; index < args.length; index++) {
+        let argsKey = origArray.indexOf(args[index]);
+        if (argsKey !== -1) {
+            origArray.splice(argsKey, 1);
+        }
+    }
+    // console.log(origArray);
+    return origArray;
 };
 
+// removeFromArray([1,2,3,4], 2, 4);
 // Do not edit below this line
 module.exports = removeFromArray;
