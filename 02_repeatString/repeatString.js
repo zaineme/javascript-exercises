8c08ef993bbf629570bf7859719919a994b84d41
zaineme <zaineraybautista@gmail.com>
2023-07-07 18:30:18 +0800

    repeatString commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text, times) {
+    let result = '';
+    if (text && times > 0) {
+        result = (text).repeat(times);
+    } else if (times < 0) {
+        result = 'ERROR';
+    }
+    return result;
 };
+
+repeatString('test', 12);
 
 // Do not edit below this line
 module.exports = repeatString;
