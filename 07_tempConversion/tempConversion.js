3d04ccbcff6a42b52f984560087b37d8012bcae6
zaineme <zaineraybautista@gmail.com>
2023-07-17 20:52:37 +0800

    tempConversion commit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  let celsius = (number - 32) / 1.8;
+  if (!Number.isInteger(celsius)) {
+    celsius = +celsius.toFixed(1);
+  }
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  let fahrenheit = (number * 1.8) + 32;
+  if (!Number.isInteger(fahrenheit)) {
+    fahrenheit = +fahrenheit.toFixed(1);
+  }
+  return fahrenheit;
 };
 
 // Do not edit below this line
