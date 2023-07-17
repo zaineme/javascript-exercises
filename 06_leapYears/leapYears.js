fe42ced781d6c04363292c1efcd891d39c9e5611
zaineme <zaineraybautista@gmail.com>
2023-07-17 20:30:03 +0800

    leapYears commit

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let result = false;
+    if ((year % 4 == 0 && year % 100 != 0)
+    || (year % 100 == 0 && year % 400 == 0)) {
+        result = true;
+    }   
+    return result;
 };
 
 // Do not edit below this line
