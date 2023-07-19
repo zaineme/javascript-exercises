2a03b6e42a2dc254f0d3a63935f1a178d713bc6e
zaineme <zaineraybautista@gmail.com>
2023-07-19 20:37:07 +0800

    palindrome Commit

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // Word only, alphanumeric
+   let re = /[\W]/g;
+   let processedString = string.toLowerCase().replace(re, '');
+   let reversedString = processedString.split('').reverse().join('');
+   return processedString === reversedString;
 };
 
 // Do not edit below this line
