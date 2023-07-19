edf55fa8daa58b1949cdc764b00242a8630de3a2
zaineme <zaineraybautista@gmail.com>
2023-07-19 22:40:54 +0800

    getTheTitles Commit

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = [];
+    for (i in books) {
+        titles.push(books[i]['title']);
+    }
+    return titles;
 };
 
 // Do not edit below this line
