f4de86ab1fd7ced9ec9372b26ef790d29422c273
zaineme <zaineraybautista@gmail.com>
2023-07-19 20:02:30 +0800

    calculator Commit

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	return args.reduce((partialSum, currentValue) => partialSum + currentValue, 0);
+  
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  return args.reduce((partialValue, currentValue) => partialValue * currentValue);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+function recurseFactorial(num) {
+  if (num == 0) {
+    return 1;
+  } else {
+    return num * recurseFactorial(num - 1);
+  }
+}
+
+const factorial = function(number) {
+  return recurseFactorial(number);
 };
 
 // Do not edit below this line
