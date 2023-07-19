d95cf957fb96d2a6dbd141f2f361c1dd753c5c36
zaineme <zaineraybautista@gmail.com>
2023-07-19 22:33:03 +0800

    fibonacci Commit

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0 ) {
+        return 'OOPS';
+    } 
 
+    // we can also use array to store prev and next
+    let prevNumber = 1;
+    let nextNumber = 1;
+    let tempNumber = 0;
+    for (let index = 1; index < number; index++) {
+        nextNumber = prevNumber + tempNumber;
+        tempNumber = prevNumber;
+        prevNumber = nextNumber;
+    }
+    return nextNumber;
 };
 
 // Do not edit below this line
