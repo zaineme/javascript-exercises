59c42a1d113cca570f2cff12bd2138771d6172da
zaineme <zaineraybautista@gmail.com>
2023-07-21 22:28:41 +0800

    findTheOldest Commit

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  
+  let age = 0;
+  let tempAge = 0;
+  let birthYear = '';
+  let deathYear = new Date().getFullYear(); 
+  let result = {};
+  for (let i in people) {
+    if (people[i]['yearOfDeath'] !== undefined) {
+      deathYear = people[i]['yearOfDeath'];
+    }
+    tempAge = deathYear -  people[i]['yearOfBirth'];
+    if (tempAge > age) {
+      age = tempAge;
+      result.name = people[i]['name'];
+    }
+  }
+  return result;
 };
-
+  
 // Do not edit below this line
 module.exports = findTheOldest;
